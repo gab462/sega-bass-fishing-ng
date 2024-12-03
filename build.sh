c6735331daba8f74e1ac5b51f2e1e35a43f5135f
gab462 <62474485+gab462@users.noreply.github.com>
2024-12-03 10:03:27 -0300

    Enable more optimizations

--- build.sh
+++ build.sh
@@ -11,11 +11,15 @@ set -xe
     popd && popd
 
 [ ! -f ./libolive.a ] &&
-    cc -std=c99 -DOLIVEC_IMPLEMENTATION -DOLIVECDEF="" -c ./external/olive.c -o ./olive.o &&
+    cc -std=c99 -O2 -DOLIVEC_IMPLEMENTATION -DOLIVECDEF="" -c ./external/olive.c -o ./olive.o &&
     ar rcs ./libolive.a ./olive.o
 
 [ ! -f ./libmsf_gif.a ] &&
-    cc -std=c99 -x c -DMSF_GIF_IMPL -c ./external/msf_gif.h -o ./msf_gif.o &&
+    cc -std=c99 -O2 -x c -DMSF_GIF_IMPL -c ./external/msf_gif.h -o ./msf_gif.o &&
     ar rcs ./libmsf_gif.a ./msf_gif.o
 
-c++ -std=c++23 -Wall -Wextra -I ./external/DPP/build/install/include/ -I ./external main.cpp -o main -L . -l:libolive.a -l:libmsf_gif.a -L ./external/DPP/build/install/lib/ -ldpp -Wl,-rpath ./external/DPP/build/install/lib/ -ggdb
+c++ -std=c++23 -Wall -Wextra -O2 \
+    -I ./external/DPP/build/install/include/ -I ./external \
+    main.cpp -o main \
+    -L . -l:libolive.a -l:libmsf_gif.a -L ./external/DPP/build/install/lib/ -ldpp \
+    -Wl,-rpath ./external/DPP/build/install/lib/
